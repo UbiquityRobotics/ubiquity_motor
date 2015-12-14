7967980ac8675a047e9b331ba1fbc77e11b0a08d
Rohan Agrawal <send2arohan@gmail.com>
2015-12-14 11:54:29 -0800

    more comments in the code

--- include/ubiquity_motor/motor_serial.h
+++ include/ubiquity_motor/motor_serial.h
@@ -43,7 +43,10 @@ class MotorSerial
 		MotorSerial(const std::string& port = "/dev/ttyUSB0" , uint32_t baud_rate = 9600, double loopRate = 100);
 		~MotorSerial();
 
+		//locking mutex for the input queue
 		boost::mutex input_mtx_;
+
+		//locking mutex for the output queue
 		boost::mutex output_mtx_;
 		
 		int transmitCommand(MotorMessage command);
@@ -55,10 +58,14 @@ class MotorSerial
 		
 		std::string _port;
 		uint32_t _baud_rate;
-                
+
+		// bool to check for input to avoid unnecessary locking                
 		bool have_input;
-		std::queue<MotorMessage> input;
-		std::queue<MotorMessage> output;
+		// queue for messages that are to be transmitted
+		std::queue<MotorMessage> input; 
+
+		//queue for messages that have been received
+		std::queue<MotorMessage> output; 
 
 		boost::thread* serial_thread;
 		ros::Rate* serial_loop_rate;
@@ -67,6 +74,7 @@ class MotorSerial
 		MotorMessage getInputCommand();
 		void appendOutput(MotorMessage command);
 
+		// Thread that has manages the serial port 
 		void SerialThread();
 };
 
