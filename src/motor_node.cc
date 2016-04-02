426230f669be29962b0176516bb2deb53e231740
Rohan Agrawal <send2arohan@gmail.com>
2016-04-02 08:37:00 -0700

    cleanup code in motor_node

--- src/motor_node.cc
+++ src/motor_node.cc
@@ -37,24 +37,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ros/ros.h>
 
 static const double BILLION = 1000000000.0;
-struct timespec last_time;
-struct timespec current_time;
-
-// void controlLoop(
-// 	MotorHardware &robot,
-// 	controller_manager::ControllerManager &cm,
-// 	timespec &last_time,
-// 	timespec &current_time){
-	
-// 	clock_gettime(CLOCK_MONOTONIC, &current_time);
-// 	ros::Duration elapsed = ros::Duration(current_time.tv_sec - last_time.tv_sec + (current_time.tv_nsec - last_time.tv_nsec) / BILLION);
-// 	last_time = current_time;
-// 	robot.sendPid();
-// 	robot.readInputs();
-// 	cm.update(ros::Time::now(), elapsed);
-// 	robot.writeSpeeds();	
-
-// }
 
 main(int argc, char* argv[]) {
 	ros::init(argc, argv, "motor_node");
@@ -70,26 +52,22 @@ main(int argc, char* argv[]) {
 	int32_t pid_derivative;
 	int32_t pid_denominator;
 
-	if (!nh.getParam("ubiquity_motor/pid_proportional", pid_proportional))
-	{
+	if (!nh.getParam("ubiquity_motor/pid_proportional", pid_proportional)) {
 		pid_proportional = 450;
 		nh.setParam("ubiquity_motor/pid_proportional", pid_proportional);
 	}
 
-	if (!nh.getParam("ubiquity_motor/pid_integral", pid_integral))
-	{
+	if (!nh.getParam("ubiquity_motor/pid_integral", pid_integral)) {
 		pid_integral = 120;
 		nh.setParam("ubiquity_motor/pid_integral", pid_integral);
 	}
 
-	if (!nh.getParam("ubiquity_motor/pid_derivative", pid_derivative))
-	{
+	if (!nh.getParam("ubiquity_motor/pid_derivative", pid_derivative)) {
 		pid_derivative = 70;
 		nh.setParam("ubiquity_motor/pid_derivative", pid_derivative);
 	}
 
-	if (!nh.getParam("ubiquity_motor/pid_denominator", pid_denominator))
-	{
+	if (!nh.getParam("ubiquity_motor/pid_denominator", pid_denominator)) {
 		pid_derivative = 1000;
 		nh.setParam("ubiquity_motor/pid_denominator", pid_denominator);
 	}
@@ -98,8 +76,7 @@ main(int argc, char* argv[]) {
 	robot.sendPid();
 	
 	double controller_loop_rate;
-	if (!nh.getParam("ubiquity_motor/controller_loop_rate", controller_loop_rate))
-	{
+	if (!nh.getParam("ubiquity_motor/controller_loop_rate", controller_loop_rate)) {
 		controller_loop_rate = 10;
 		nh.setParam("ubiquity_motor/controller_loop_rate", controller_loop_rate);
 	}
