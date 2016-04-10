ffaaa3fc535b7cb93a5bf0b5aa6ec208f2b67a60
Rohan Agrawal <send2arohan@gmail.com>
2016-04-10 09:56:58 -0700

    Make sure exception is thrown for bad port name

--- test/motor_serial_test.cc
+++ test/motor_serial_test.cc
@@ -70,16 +70,24 @@ protected:
 };
 
 TEST(MotorSerialNoFixtureTests, badPortnameException) {
+  bool exception = false;
   try { 
     //delete motors; 
     MotorSerial motors(std::string("foo"), 9600, 1000);
   }
   catch(serial::IOException) {
+    exception = true;
     SUCCEED();
   }
   catch(...){
+    exception = true;
     FAIL();
   }
+
+  if (exception == false) {
+    FAIL() << "No exception thrown for bad port name";
+  }
+  
 }
 
 TEST_F(MotorSerialTests, serialClosedOnInterupt) {
