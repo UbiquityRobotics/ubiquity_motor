cb8a9893e140b9859eb403b29d3c9ce738c72285
Rohan Agrawal <send2arohan@gmail.com>
2016-04-24 07:00:14 -0700

    print outputs on debug instead of error

--- src/motor_serial.cc
+++ src/motor_serial.cc
@@ -230,7 +230,7 @@ void MotorSerial::SerialThread() {
 				std::vector<uint8_t> out(8);
 
 				out = getInputCommand().serialize();
-				ROS_ERROR("out %x %x %x %x %x %x %x %x",
+				ROS_DEBUG("out %02x %02x %02x %02x %02x %02x %02x %02x",
 				 	out[0],
 				 	out[1],
 				 	out[2],
