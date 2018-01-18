e1527f7f1cc3164e254737a14908391c46d42308
Rohan Agrawal <send2arohan@gmail.com>
2018-01-17 18:16:48 -0800

    add support for getting robot_id

--- src/robot_id.cc
+++ src/robot_id.cc
@@ -33,11 +33,12 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 struct Options
 {
-	bool robot;
-	bool firmware;
-	bool pcb_rev;
-	bool verbose;
-    bool help;
+	bool robot = false;
+	bool firmware = false;
+	bool pcb_rev = false;
+	bool verbose = false;
+    bool help = false;
+    std::string serial_port = "";
 };
 
 bool find_switch(const std::vector<std::string>& args, 
@@ -47,6 +48,19 @@ bool find_switch(const std::vector<std::string>& args,
     bool has_long = (std::find(args.cbegin(), args.cend(), long_sw)) != args.cend();
 
     return (has_long || has_short);
+}
+
+std::string get_option(const std::vector<std::string>& args, 
+    const std::string& option, const std::string& default_val) {
+
+    auto opt = std::find(args.cbegin(), args.cend(), option);
+
+    if (opt != args.cend() && opt++ != args.cend()) {
+        return *(opt++);
+    }
+    else {
+        return default_val;
+    }
 }
 
 Options parse_args(const std::vector<std::string>& args) {
@@ -64,6 +78,8 @@ Options parse_args(const std::vector<std::string>& args) {
         op.pcb_rev = true;
     }
 
+    op.serial_port = get_option(args, "-D", "/dev/ttyAMA0");
+
     return op;
 }
 
@@ -75,20 +91,86 @@ int main(int argc, char const *argv[])
 
     if (op.help) {
         printf("%s\n", "Used to probe Ubiquity Robotics robots for version info");
+        printf("%s\n", "Options:");
+        printf("%s\n", "  -D {PATH TO PORT}: Set the Serial Port to use (e.g. /dev/ttyAMA0)");
         printf("%s\n", "Switches:");
-        printf("%s\n", "-a --all: Print out all options.");
-        printf("%s\n", "-r --robot: Print out robot platform (e.g. loki, magni, none)");
-        printf("%s\n", "-f --firmware: Print out firmware revision");
-        printf("%s\n", "-p --pcb: Print out the PCB revision");
-        printf("%s\n", "-v --verbose: Print out debugging information.");
-        printf("%s\n", "-h --help: Print out this help");
+        printf("%s\n", "  -a --all: Print out all options.");
+        printf("%s\n", "  -r --robot: Print out robot platform (e.g. loki, magni, none)");
+        printf("%s\n", "  -f --firmware: Print out firmware revision");
+        printf("%s\n", "  -p --pcb: Print out the PCB revision");
+        printf("%s\n", "  -v --verbose: Print out debugging information.");
+        printf("%s\n", "  -h --help: Print out this help");
+
+        return 0;
     }
 
-    fprintf(stderr, "Robot ID: %d\n", op.robot);
-    fprintf(stderr, "Firmware: %d\n", op.firmware);
-    fprintf(stderr, "PCB: %d\n", op.pcb_rev);
-    fprintf(stderr, "Verbose: %d\n", op.verbose);
-    fprintf(stderr, "Help: %d\n", op.help);
+    if (op.verbose) {
+        fprintf(stderr, "%s\n", "Parsed Args:");
+        fprintf(stderr, "  Robot ID: %d\n", op.robot);
+        fprintf(stderr, "  Firmware: %d\n", op.firmware);
+        fprintf(stderr, "  PCB: %d\n", op.pcb_rev);
+        fprintf(stderr, "  Verbose: %d\n", op.verbose);
+        fprintf(stderr, "  Help: %d\n", op.help);
+        fprintf(stderr, "  Serial Port: %s\n", op.serial_port.c_str());
+    }
+
+    try {
+        serial::Serial robot(op.serial_port, 115200, serial::Timeout::simpleTimeout(100));
+
+        if (op.robot) {
+            MotorMessage robot_id;
+            robot_id.setRegister(MotorMessage::REG_ROBOT_ID);
+            robot_id.setType(MotorMessage::TYPE_READ);
+            robot_id.setData(0);
+
+            RawMotorMessage out = robot_id.serialize();
+            robot.write(out.c_array(), out.size());
+
+            robot.flush();
+
+            bool timedout = false;
+            struct timespec start, current_time;
+            clock_gettime(CLOCK_MONOTONIC, &current_time);
+            start = current_time;
+
+            while (robot.isOpen() && !timedout) {
+                if (robot.waitReadable()) {
+                    RawMotorMessage innew = {0, 0, 0, 0, 0, 0, 0, 0};
+
+                    robot.read(innew.c_array(), 1);
+                    if (innew[0] != MotorMessage::delimeter) continue;
+
+                    robot.waitByteTimes(innew.size()-1);
+                    robot.read(&innew.c_array()[1], 7);
+
+                    MotorMessage rsp;
+                    rsp.deserialize(innew);
+
+                    if (rsp.getData() == 0x00) {
+                        printf("%s\n", "magni");
+                    }
+                    if (rsp.getData() == 0x01) {
+                        printf("%s\n", "loki");
+                    }
+
+                    break;
+                }
+
+                clock_gettime(CLOCK_MONOTONIC, &current_time);
+                timedout = (current_time.tv_sec >= start.tv_sec + 5);
+            }
+
+            if (timedout) {
+                if (op.verbose) {fprintf(stderr, "%s\n", "Timed out waiting for response");}
+                printf("%s\n", "none");
+                return 2;
+            }
+        }
+    }
+    catch (const serial::IOException& e) {
+        if (op.verbose) {fprintf(stderr, "Error opening Serial Port %s\n", op.serial_port.c_str());}
+        return 1;
+    }
 	
 	return 0;
 }
